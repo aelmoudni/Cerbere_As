910b3ea8eae47b28e45f4ebdfa3e1243e4de883e
aelmoudni <luther.kingas@gmail.com>
2022-03-30 16:35:38 +0200

    lecture_opt supprime les frames à moins de 5 events

--- docadage/lecture_optim.py
+++ docadage/lecture_optim.py
@@ -1,5 +1,6 @@
 import glob
 from unittest import result
+from cv2 import dft
 from pylab import *
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -7,7 +8,7 @@ import imageio
 import os
 #some imports aren't necessary, i used them just for several test i ve done 
 
-files = sorted(glob.glob('out_datawalking/*')) #specify the path if the out_data file isn't in the same file 
+files = sorted(glob.glob('/Users/aelmoudn/Desktop/Anass_Cerbere/docadage/outputs_caiman/out_datawalking/*')) #specify the path if the out_data file isn't in the same file 
 images = []
 for file in files:
     with open(file, 'r') as inf:
@@ -15,12 +16,15 @@ for file in files:
         resultx=[]
         resulty=[]
         polarite=[]
+        frame_event_index=0
+
         for x in lines: #for each line, we get the 2nd columln for x, 3rd for y and 5th for the polarity 
             resultx.append(x.split()[2])
             resulty.append(x.split()[3])
             polarite.append(x.split()[5])
+            frame_event_index=frame_event_index+1
 
-            #print(x)
+            print(x)
         inf.close()
         coordx = []
         coordy = []
@@ -40,9 +44,12 @@ for file in files:
             img.save('sqr.png')
 
             return img
-    wallpaper = newImg()
-    #wallpaper.show() 
-    images.append(wallpaper)  
+    if frame_event_index > 5 : 
+
+        wallpaper = newImg()
+        #wallpaper.show() 
+        images.append(wallpaper)  
+ 
 
 imageio.mimsave(os.path.join('moviewalkingman.gif'), images, duration = 0.04) # modify duration as needed
 
